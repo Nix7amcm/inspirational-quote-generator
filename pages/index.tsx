ca153c9df0c6dfd59bac9df9047a7796dd319b53
Nix7amcm <65091582+Nix7amcm@users.noreply.github.com>
2023-05-30 19:52:09 +0100

    Add gradient bg container sc

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+export default function Home () {
+  return (
+    <>
+      <Head>
+        <title>Inspirational Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/*Background*/}
+      <GradientBackgroundCon>
+
+      </GradientBackgroundCon>
+
+    </>
+  )
+}
